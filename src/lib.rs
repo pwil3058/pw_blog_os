29169d674284fea2e8a6e7da282c827e65e034fa
Peter Williams <pwil3058@bigpond.net.au>
2021-07-02 15:07:52 +1000

    Heap Allocation

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,13 @@
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
@@ -20,6 +24,11 @@ pub fn init() {
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
 
 pub trait Testable {
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@
 #![test_runner(pw_blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::boxed::Box;
+use alloc::rc::Rc;
+use alloc::vec;
+use alloc::vec::Vec;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use pw_blog_os::println;
@@ -13,22 +19,42 @@ use pw_blog_os::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use pw_blog_os::allocator;
     use pw_blog_os::memory::{self, BootInfoFrameAllocator};
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::VirtAddr;
 
     println!("Hello World{}", "1");
 
     pw_blog_os::init();
 
-    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    // let mut fram_allocater = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocater = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    //let page = Page::containing_address(VirtAddr::new(0xdeadbeef000));
-    //memory::create_example_mapping(page, &mut mapper, &mut fram_allocater);
+    allocator::init_heap(&mut mapper, &mut frame_allocater).expect("heap initialization failed");
 
-    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) }
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
